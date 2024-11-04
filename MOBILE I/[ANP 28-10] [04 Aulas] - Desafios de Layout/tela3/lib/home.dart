71e3db59c9ee8100a829a9c92d5c8ce0c1679ebf
Kauê Rian Silva Conceição Oliveira <06580987219@pma.arq>
2024-11-04 14:18:42 -0400

    Concluindo a tela 3

--- MOBILE I/[ANP 28-10] [04 Aulas] - Desafios de Layout/tela3/lib/home.dart
+++ MOBILE I/[ANP 28-10] [04 Aulas] - Desafios de Layout/tela3/lib/home.dart
@@ -11,21 +11,36 @@ class Home extends StatelessWidget {
           centerTitle: true,
           title: Text('Jogo do Pedra, Papel e Tesoura'),
           backgroundColor: Colors.deepOrange,
-          foregroundColor: Colors.white,
+          foregroundColor: Color.fromARGB(255, 250, 250, 250),
         ),
         body: Center(
           child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextWidget(texto: 'Escolha da Máquina')
-              ]
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextWidget(texto: 'Escolha da Máquina'),
+                  IconeWidget(textoIcone: '👊'),
+                  TextWidget(texto: 'Escolha uma opção abaixo'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  IconeWidget(textoIcone: '👊'),
+                  IconeWidget(textoIcone: '🤚'),
+                  IconeWidget(textoIcone: '✌️'),
+                ],
+              ),
+              TextWidget(texto: 'Você ganhou!'),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
 
 class TextWidget extends StatelessWidget {
   final String texto;
@@ -37,12 +52,41 @@ class TextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(8),
-      child: Text(texto, style: TextStyle(fontWeight: FontWeight.bold),),
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Text(
+        texto,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ),
     );
   }
 }
 
-class IconeWidget extends StateLessWidget {
-  final 
+class IconeWidget extends StatelessWidget {
+  final String textoIcone;
+
+  IconeWidget({
+    super.key,
+    required this.textoIcone,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: Container(
+        width: 120,
+        height: 120,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Color.fromARGB(255, 241, 241, 241),
+            border: Border.all(color: Color.fromARGB(100, 0, 0, 0), width: 4)),
+        child: Text(
+          textoIcone,
+          style: TextStyle(fontSize: 64),
+        ),
+      ),
+    );
+  }
 }
